f598f75292e4116a7d4271194c1eb486954b9135
xuyunying <xuyunying@yy.com>
2020-03-08 18:48:30 +0800

    feat: 增加dev server 支持和css css module支持

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,24 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                hashPrefix: 'heihei',
+              },
+            }
+          }
+        ]
       }
     ]
   },
